9372fdcb150698f24de64e2460823a53730c1668
hamik112 <dreambigmedia12@gmail.com>
2022-02-03 00:11:17 -0800

    Legal Backend Update

--- app/app/routes/leads.py
+++ app/app/routes/leads.py
@@ -23,6 +23,7 @@ async def leads(*, request:Request, request_data: RequestsHeaders = Depends(Requ
 async def createlead(*, request:Request, request_data: RequestsHeaders = Depends(RequestsHeaders)):
 	body = await request.json()
 	logger.info(f"Leads: {body}")
+	logger.info(f" Request Data : {request_data}")
 	email = body['email']
 	phone = body['phone']
 	existing_lead = await Lead.objects.get_or_none(email = email, phone = phone)
