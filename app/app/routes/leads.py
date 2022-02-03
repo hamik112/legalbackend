dce02a81809acbc5ba8969f9247913f7d45c8eef
hamik112 <dreambigmedia12@gmail.com>
2022-02-03 00:38:04 -0800

    Legal Backend Update

--- app/app/routes/leads.py
+++ app/app/routes/leads.py
@@ -30,7 +30,7 @@ async def createlead(*, request:Request, request_data: RequestsHeaders = Depends
 
 	email = body['email']
 	phone = body['phone']
-	existing_lead = await Lead.objects.filter(email = email, phone = phone).exists()
+	existing_lead = await Lead.objects.filter(ormar.and_(email = email, phone = phone)).exists()
 	if not existing_lead:
 		if request_data.hit:
 			hit = await TrackHit.objects.get(request_data.hit)
